64c5cb255ea9149bf56903dbce4d50795be456b6
kuriharakento <peroperoperopero777@gmail.com>
2026-09-14 17:43:33 +0900

    メニューとシーンの窓と FeatureCheck を日本語にして、最初に前に出すタブを ###ID で指すようにしたよ。日本語化した engine を参照するようにした

--- application/scene/play/title/TitleScene.cpp
+++ application/scene/play/title/TitleScene.cpp
@@ -23,7 +23,7 @@ REGISTER_SCENE(TitleScene);
 void TitleScene::Initialize()
 {
 #ifdef USE_IMGUI
-	KCE::DebugUIManager::GetInstance()->RegisterHierarchySection(this, "Title Scene", [this]() { this->DrawImGui(); });
+	KCE::DebugUIManager::GetInstance()->RegisterHierarchySection(this, "タイトル", [this]() { this->DrawImGui(); });
 #endif
 
 	KCE::Object3dCommon* objCommon = sceneManager_->GetObject3dCommon();
